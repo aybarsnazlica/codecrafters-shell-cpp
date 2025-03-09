d074d9b1ee043cd0e5e5e0ad1115391f2eb25229
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-03-09 18:03:49 +0900

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 int main()
 {
@@ -12,7 +13,24 @@ int main()
         std::cout << "$ ";
         std::string input;
         std::getline(std::cin, input);
-        if (input == "exit 0") return 0;
-        std::cout << input << ": command not found\n";
+
+        std::istringstream iss(input);
+        std::string command;
+        iss >> command;
+
+        if (command == "echo")
+        {
+            std::string output;
+            std::getline(iss >> std::ws, output);
+            std::cout << output << '\n';
+        }
+        else if (command == "exit")
+        {
+            return 0;
+        }
+        else
+        {
+            std::cout << input << ": command not found\n";
+        }
     }
 }
